b5f8ad6526b77d028d1f7d0e7b4379b8a3a6fec0
Wchris95 <51712970+Wchris95@users.noreply.github.com>
2022-04-08 16:03:46 -0400

    Updated Gridworld class
    Created functions within the gridworld class
    - Actions: gives the actions the current state can currently reach
    - Check_state: checks if the state is in the terminal state
    
    These functions may need to be migrated over to the the Q agent class

--- Gridworld.py
+++ Gridworld.py
@@ -6,13 +6,14 @@ goal location, and our available actions the agent may take.
 Pass in our height and width to build grid if we 
 """
 class GridWorld:
-    def __init__(self,current_location):
+    def __init__(self,current_location,terminal_states):
         self.height = 40
         self.width = 40
         #initialize the grid as 0s
         self.grid = np.zeros((self.height, self.width)) - 1
         #set a random start location for the agent
         self.current_location = current_location
+        self.terminal_states = terminal_states
         self.actions = ['NORTH','EAST','SOUTH','WEST','NORTHEAST','NORTHWEST','SOUTHEAST','SOUTHWEST']
     
     def get_available_actions(self):
@@ -28,6 +29,62 @@ class GridWorld:
     
     def make_step(self, action):
         last_location = self.current_location
-        if action == 'North':
-            
+        if action == 'NORTH':
+            if last_location[0] == 0:
+                reward = self.get_reward(self.current_location)
+            else:
+                self.current_location = (self.current_location[0]+1, self.current_location[1])
+                reward = self.get_reward(self.current_location)
+        elif action == 'NORTHEAST':
+            if last_location[0] == 0 or last_location[1] == self.width-1:
+                reward = self.get_reward(self.current_location)
+            else:
+                self.current_location = (self.current_location[0]+1, self.current_location[1]+1)
+                reward = self.get_reward(self.current_location)
+        elif action == 'NORTHWEST':
+            if last_location[0] == 0 or last_location[1] == 0:
+                reward = self.get_reward(self.current_location)
+            else:
+                self.current_location = (self.current_location[0]+1, self.current_location[1]-1)
+                reward = self.get_reward(self.current_location)
+        elif action == 'SOUTH':
+            if last_location[0] == self.height-1:
+                reward = self.get_reward(self.current_location)
+            else:
+                self.current_location = (self.current_location[0]-1, self.current_location[1])
+                reward = self.get_reward(self.current_location)
+        elif action == 'SOUTHEAST':
+            if last_location[0] == self.height-1 or last_location[1] == self.width-1:
+                reward = self.get_reward(self.current_location)
+            else:
+                self.current_location = (self.current_location[0]-1, self.current_location[1]+1)
+                reward = self.get_reward(self.current_location)
+        elif action == 'SOUTHWEST':
+            if last_location[0] == self.height-1 or last_location[1] == 0:
+                reward = self.get_reward(self.current_location)
+            else:
+                self.current_location = (self.current_location[0]-1, self.current_location[1]-1)
+                reward = self.get_reward(self.current_location)
+        elif action == 'WEST':
+            if last_location[1] == 0:
+                reward = self.get_reward(self.current_location)
+            else:
+                self.current_location = (self.current_location[0]+1, self.current_location[1])
+                reward = self.get_reward(self.current_location)
+        elif action == 'EAST':
+            if last_location[0] == self.width-1:
+                reward = self.get_reward(self.current_location)
+            else:
+                self.current_location = (self.current_location[0]+1, self.current_location[1])
+                reward = self.get_reward(self.current_location)
+        return reward
+    
+    #Checks if our current state is a terminal state based on a defined terminal state
+    """
+        unsure if this function is needed as unsure how this is determined and if it is not
+        given how is it given from the API will the state we find just return terminal?
+    """
+    def check_state(self):
+        if self.current_location in self.terminal:
+            return 'TERMINAL'
         
--- QlearnAgent.py
+++ QlearnAgent.py
@@ -3,20 +3,33 @@ Our Q agent be the agent exporing the gridworld that will track the current
 Q table values, the rewards received after an action, actions the agent currently
 can take, and the location the agent currently is in. We will have the learning function
 set within our Q Agent as it traverses through the grid world
+Possible changes:
+    we may need to have a attribute of last location or funciton for 
+    next moves in this class vs within the Gridworld
+    class. (same for when we check the state if it is termianl)
+    Dependent on what is given to us from the API
 """
 import numpy as np
 
 class QAgent:
-    def __init__(self, alpha, gamma, location_state, actions, rewards, state_to_location):
+    def __init__(self, location_state, actions, rewards, state_to_location, alpha=0.1, gamma=1, epsilon = 0.05):
+        if not(0< gamma <= 1):
+            raise ValueError("Gamma must be 0 < gamma <= 1")
         self.alpha = alpha
         self.gamma = gamma
+        self.epsilon = epsilon
         self.location_state = location_state
         self.actions = actions
-        self.rewards = rewards
+        self.rewards = rewards or {state: 0 for state in self.states}
         self.state_to_location = state_to_location
         #initializing the Q learning table
         loc_state_length = len(location_state)
         self.Q = np.zeros((loc_state_length,loc_state_length), dtype = None, order = 'C')
         
-    def training(self, start_location, end_location, iterations)
+    def training(self, start_location, end_location, iterations):
+        rewards_new = np.copy(self.rewards)
+        ending_state = self.location_state[end_location]
+        rewards_new[ending_state, ending_state] = 999
         
+        for i in range(iterations):
+            current_state = np.random.randint()
