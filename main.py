e91d70de0a5a69a2a1d27e4321ea34bc8895a4bd
Wchris95 <51712970+Wchris95@users.noreply.github.com>
2022-04-10 11:34:56 -0400

    Added Play function and added notes
    Added notes on possible changes needed to include the need of the API call. Currently have play function when deciding action and when to call the learn function in agent, but may be better to extract this to main

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
 """
-main script that will grab the gridWorld from the API for the environment input
-in the play function 
+Add Play function
+input: environment, agent, number of trials, and
+the max amount of steps it go through until it ends the process or until the game has reached a
+terminal state
+outputs: reward per episode
+Possibly extract the play function out and just place into our main call 
+this way we have more control over when to make the next action or get the score
 """
+import time
 
+def play(environment, agent, trials, maxSteps):
+    episodeRewards = []
+    
+    for trial in range(trials):
+        totalReward = 0
+        step = 0
+        terminate = False
+        while step < maxSteps and terminate != True:
+            previousState = environment.current_location
+            action = agent.actionSelection
+            #set timer for 15 seconds before next step to API
+            time.sleep(15)
+            reward = environment.make_step(action)
+            #call API pass action taken to make step
+            """Place Make a move api call here"""
+            newCurrentState = environment.current_location
+            agent.learn(previousState, reward, newCurrentState, action)
+            totalReward += reward
+            step+=1
+            if environment.check_state == 'TERMINAL':
+                environment.__init__()
+                terminate = True
+        episodeRewards.append(totalReward)
+    return episodeRewards
+
+if __name__ == "__main__":
+    #Use the API to pull the Gridworld first
+    """Enter a world API call"""
+    """grab the starting state from the return of the API call"""
+    """Pass in the world and starting state into the play function"""
+    
--- playFunc.py
+++
@@ -1,7 +0,0 @@
-"""
-Add Play function
-input: environment, agent, number of trials, and
-the max amount of steps it go through until it ends the process or until the game has reached a
-terminal state
-outputs: reward per episode
-"""
